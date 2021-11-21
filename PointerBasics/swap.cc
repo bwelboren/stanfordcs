cc77e472c55f0bc094ab9da5ccc769605ab08855
Björn Welboren <52087548+bwelboren@users.noreply.github.com>
2021-11-21 15:31:35 +0100

    Update swap.cc

--- PointerBasics/swap.cc
+++ PointerBasics/swap.cc
@@ -2,14 +2,15 @@
 If you declare 'pass by reference' (&) as parameters,
 the compiler will take care of changing the values globally.
 As you can see it succesfully changed the values in t_swapCaller without using dereference (*)
+We also didnt have to pass the values as &a and &b, compiler did that for us aswell
 **/
 
 void t_Swap(int &a, int &b){
 	int temp;
 	temp = b;
 
-  b = a;
-  a = temp; 
+  	b = a;
+  	a = temp; 
 }
 
 int t_swapCaller(){
